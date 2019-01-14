72f084a88c153d1b31e570b8f831ec9d386e401a
Elisha Misoi <e.kmisoi@outlook.com>
2019-01-14 18:22:49 +0300

    [Finishes #163188807] added CORS support

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from app.db import create_tables
 from flask import jsonify
 from flask_cors import CORS
 
+create_tables.create_tables()
 create_tables.create_super_admin()
 app = Flask(__name__)
 CORS(app)
