9754798b3f94293edcb926181130f80eef53da27
Elisha-Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2019-01-14 19:10:20 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,9 @@ from app.db import create_tables
 from flask import jsonify
 from flask_cors import CORS
 
-create_tables.drop_tables()
-# create_tables.create_tables()
-# create_tables.create_super_admin()
+
+create_tables.create_tables()
+create_tables.create_super_admin()
 app = Flask(__name__)
 CORS(app)
 
